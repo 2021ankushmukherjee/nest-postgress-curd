d2f42ecdbde4c44b455b448a235b8cd8ed6e0078
Ankush Mukherjee <ankush.mukherjee24@gmail.com>
2023-06-26 18:31:36 +0530

    curd operation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: "./config/config.env"
+  }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'test-postgres',
+    password: 'postgres',
+    database: 'mytestdb',
+    entities: [__dirname + "/**/*.entity{.ts,.js}"],
+    synchronize: true, 
+    logging:true /* show logs */}),
+  UserModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
